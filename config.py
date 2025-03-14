c3ab88e7a15483d57f0b24b0172ee7f490edee0a
iFuxu <109459544+iFuxu@users.noreply.github.com>
2025-03-14 14:38:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ PUSHPLUS_TOKEN = "" or os.getenv("PUSHPLUS_TOKEN")
 TELEGRAM_BOT_TOKEN = "" or os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = "" or os.getenv("TELEGRAM_CHAT_ID")
 # wxpusher推送时需填
-WXPUSHER_SPT = "" or os.getenv("WXPUSHER_SPT")  
+WXPUSHER_SPT = "" or os.getenv("WXPUSHER_SPT")
 # read接口的bash命令，本地部署时可对应替换headers、cookies
 curl_str = os.getenv('WXREAD_CURL_BASH')
 
@@ -34,32 +34,30 @@ cookies = {
 
 headers = {
     'accept': 'application/json, text/plain, */*',
-    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5',  
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5',
     'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=1ff5a0725f8841088b42f97109c45862',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
 }
-
 
 """
 建议保留区域|默认读三体，其它书籍自行测试时间是否增加
 """
 data = {
-    "appId": ""
-    "b": ""
-    "c": ""
-    "ci": 70    
-    "co": 0 
-    "sm": ""
-    "pr": 74
-    "rt": 30
-    "ts": 1727660516749 
-    "rn": 31
-    "sg": ""    
-    "ct": 1727660516
-    "ps": ""
-    "pc": ""
+    "appId": "wb182564874663h152492176",
+    "b": "ce032b305a9bc1ce0b0dd2a",
+    "c": "7cb321502467cbbc409e62d",
+    "ci": 70,
+    "co": 0,
+    "sm": "[插图]第三部广播纪元7年，程心艾AA说",
+    "pr": 74,
+    "rt": 30,
+    "ts": 1727660516749,
+    "rn": 31,
+    "sg": "991118cc229871a5442993ecb08b5d2844d7f001dbad9a9bc7b2ecf73dc8db7e",
+    "ct": 1727660516,
+    "ps": "b1d32a307a4c3259g016b67",
+    "pc": "080327b07a4c3259g018787",
 }
-
 
 def convert(curl_command):
     """提取bash接口中的headers与cookies
