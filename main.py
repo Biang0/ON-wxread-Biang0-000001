d82b3d74abe0abb97c23a969385c833ca9926bde
iFuxu <109459544+iFuxu@users.noreply.github.com>
2025-03-14 13:39:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hashlib
 import requests
 import urllib.parse
 from push import push
-from config import data, headers, cookies, READ_NUM, PUSH_METHOD  
+from config import data, headers, cookies, READ_NUM, PUSH_METHOD
 
 # 配置日志格式
 logger = logging.getLogger(__name__)
